b352edc206cfa41fdcdd97af73066530162390ea
santosh spring <santosh.spring32@gmail.com>
2015-05-11 15:15:17 +0530

    added spring-boot-batch

---
+++ spring-poc/spring-boot-batch/src/main/java/com/santosh/poc/batch/Application.java
@@ -0,0 +1,39 @@
+package com.santosh.poc.batch;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... strings) throws Exception {
+
+        List<Person> persons = jdbcTemplate.query("SELECT first_name, last_name FROM people", new RowMapper<Person>() {
+            @Override
+            public Person mapRow(ResultSet rs, int row) throws SQLException {
+                return new Person(rs.getString(1), rs.getString(2));
+            }
+        });
+
+        for (Person person : persons) {
+            System.out.println("Found <" + person + "> in the database.");
+        }
+
+    }
+}
